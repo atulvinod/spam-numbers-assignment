6e1f54d6acf6a6e599ed8872641019a7e1a84241
atulvinod <atulvinod1911@gmail.com>
2024-04-08 19:03:09 +0530

    fix auth token error

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,27 +3,51 @@ import {ExtractJwt} from 'passport-jwt';
 import envVars from '@src/constants/envVars';
 import passport from 'passport';
 import * as userService from '@src/services/user.service';
+import { RouteError } from "@src/other/classes";
+import HttpStatusCodes from "@src/constants/httpStatusCodes";
 
-
-const opts =  {
-    jwtFromRequest:  ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey : envVars.jwt.secret,
+const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: envVars.jwt.secret,
     issuer: envVars.jwt.issuer,
     audience: envVars.jwt.audience,
 };
 
-passport.use(new JwtStrategy(opts, async (jwt_payload : {id: number}, done)=>{
-    try{
-        const user = await userService.getUserById(jwt_payload.id);
-        if (user.isRegisteredUser) {
-            done(null, user);
-        } else {
-            done(new Error("Not a registered user"), false);
-        }
-    }catch(error){
-        done(error, false);
-    }
-}));
+passport.use(
+    new JwtStrategy(
+        opts,
+        async (
+            jwt_payload: { phoneNumber: string; countryCode: string },
+            done,
+        ) => {
+            try {
+                const user = await userService.getUserByPhoneNumber(
+                    jwt_payload.phoneNumber,
+                    jwt_payload.countryCode,
+                );
+                if (user.isRegisteredUser) {
+                    done(null, user);
+                } else {
+                    done(
+                        new RouteError(
+                            HttpStatusCodes.UNAUTHORIZED,
+                            "Unauthorized token",
+                        ),
+                        false,
+                    );
+                }
+            } catch (error) {
+                done(
+                    new RouteError(
+                        HttpStatusCodes.UNAUTHORIZED,
+                        "Unauthorized token",
+                    ),
+                    false,
+                );
+            }
+        },
+    ),
+);
 
 const auth  = passport.authenticate("jwt", { session: false });
 export default auth;
